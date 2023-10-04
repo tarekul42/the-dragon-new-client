69da51cc46fe35a9f165c70d10d023992d7110f5
tarekul42 <tarekulrifat205@gmail.com>
2023-10-04 16:20:50 -0700

    Login and Register Form

---
+++ src/pages/Login/Register/Register.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+    return (
+        <Container className='w-50 mx-auto'>
+            <h2>Register Your Account</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="name" name='name' placeholder="Enter name" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Photo URL</Form.Label>
+                    <Form.Control type="text" name='photo' placeholder="Enter photo URL" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" name='accept' label="Accept Terms and Conditions" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+                <br />
+                <Form.Text>Already have an account? <Link to='/login'>Login</Link></Form.Text>
+                <Form.Text className='text-success'>
+
+                </Form.Text>
+                <Form.Text className='text-danger'>
+
+                </Form.Text>
+            </Form>
+        </Container>
+    );
+};
+
+export default Register;
--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,15 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../Providers/AuthProvider';
-import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
-
-    const { user } = useContext(AuthContext);
 
     return (
         <Container className='mt-4'>
@@ -24,35 +19,6 @@ const Header = () => {
                     I can be a React component, multiple React components, or just some text.
                 </Marquee>
             </div>
-            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Nav.Link href="#home">
-                                <Link to='/'>Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
-                            <Nav.Link href="#pricig">Career</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            {user && <Nav.Link href="#deets">
-                                <FaUserCircle style={{ fontSize: '2rem' }} />
-                            </Nav.Link>}
-                            <Nav.Link eventKey={2} href="#memes">
-                                {
-                                    user ? 
-                                    <Button variant='secondary'>Log Out</Button>
-                                    :
-                                    <Link to='/login'>
-                                    <Button variant='secondary'>Login</Button>
-                                    </Link>
-                                }
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
         </Container>
     );
 };
