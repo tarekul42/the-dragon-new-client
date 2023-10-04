5382c36f16ea205a527d7a6e5b3a3bd7acedcc74
tarekul42 <tarekulrifat205@gmail.com>
2023-10-04 01:37:13 -0700

    Display News Details and editors insights

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
+import NewsCard from '../NewsCard/NewsCard';
+
+const Category = () => {
+    const { id } = useParams();
+    const categoryNews = useLoaderData();
+    return (
+        <div>
+            <h2>this is a category: {categoryNews.length} </h2>
+            {
+                categoryNews.map(news => <NewsCard
+                    key={news._id}
+                    news={news}
+                ></NewsCard>)
+            }
+        </div>
+    );
+};
+
+export default Category;
